fe6b0299f65f6e55298fc65b621f19287e3190f2
DavudBaba00 <davudbaba00@gmail.com>
2023-12-13 17:32:30 +0400

    updated again

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -38,14 +38,14 @@ window.addEventListener('resize', () => {
 //////////DATA//////////
 
 let cardnum = 3;
-function showPage(){
+function showPage() {
 
-fetch('http://localhost:3000/card')
-    .then(res => res.json())
-    .then(data => {
-        console.log(data);
-        data.slice(cardnum - 3, cardnum).forEach(card => {
-            document.querySelector(".sec22").innerHTML += `
+    fetch('http://localhost:3000/card')
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            data.slice(cardnum - 3, cardnum).forEach(card => {
+                document.querySelector(".sec22").innerHTML += `
     <div>
     <img src="${card.image}" alt="">
     <a href="#"></a>
@@ -61,48 +61,117 @@ fetch('http://localhost:3000/card')
     
     </div>
     `})
-    })
+            document.querySelector("#select").addEventListener('change', (e) => {
+                if (e.target.value == "Header") {
+                    let dataS = data.sort((a, b) => {
+                        return a.link.localeCompare(b.link);
+                    });
+                    document.querySelector(".sec22").innerHTML = ''
+                    data.slice(cardnum - 3, cardnum).forEach(card => {
+                        document.querySelector(".sec22").innerHTML += `
+        <div>
+        <img src="${card.image}" alt="">
+        <a href="#"></a>
+            <h4>${card.link}</h4>
+        </a>
+        <p>${card.text}</p>
+        <span>
+            <a><button  onclick="deleteCard(${card.id})" class="delete"><i class="bi bi-trash"></i> Delete</button></a>
+            <a href="../update.html?id=${card.id}"><button class="update"><i class="bi bi-arrow-repeat"></i> Update</button></a>
+            <a href="../info.html?id=${card.id}"><button class="info"><i class="bi bi-info-circle"></i> Info</button></a>
+        </span>
+        <i class="bi bi-heart" id="heart" onclick="addFav(${card.id})"></i>
+        
+        </div>
+        `})
+                }
+                else if (e.target.value == "Text") {
+                    let dataS1 = data.sort((a, b) => {
+                        return a.text.toLowerCase().localeCompare(b.text.toLowerCase());
+                    });
+                    document.querySelector(".sec22").innerHTML = ''
+                    data.slice(cardnum - 3, cardnum).forEach(card => {
+                        document.querySelector(".sec22").innerHTML += `
+        <div>
+        <img src="${card.image}" alt="">
+        <a href="#"></a>
+            <h4>${card.link}</h4>
+        </a>
+        <p>${card.text}</p>
+        <span>
+            <a><button  onclick="deleteCard(${card.id})" class="delete"><i class="bi bi-trash"></i> Delete</button></a>
+            <a href="../update.html?id=${card.id}"><button class="update"><i class="bi bi-arrow-repeat"></i> Update</button></a>
+            <a href="../info.html?id=${card.id}"><button class="info"><i class="bi bi-info-circle"></i> Info</button></a>
+        </span>
+        <i class="bi bi-heart" id="heart" onclick="addFav(${card.id})"></i>
+        
+        </div>
+        `})
+                }
+                else if (e.target.value == 'All'){
+                    document.querySelector(".sec22").innerHTML = ''
+                    data.slice(cardnum - 3, cardnum).forEach(card => {
+                        document.querySelector(".sec22").innerHTML += `
+        <div>
+        <img src="${card.image}" alt="">
+        <a href="#"></a>
+            <h4>${card.link}</h4>
+        </a>
+        <p>${card.text}</p>
+        <span>
+            <a><button  onclick="deleteCard(${card.id})" class="delete"><i class="bi bi-trash"></i> Delete</button></a>
+            <a href="../update.html?id=${card.id}"><button class="update"><i class="bi bi-arrow-repeat"></i> Update</button></a>
+            <a href="../info.html?id=${card.id}"><button class="info"><i class="bi bi-info-circle"></i> Info</button></a>
+        </span>
+        <i class="bi bi-heart" id="heart" onclick="addFav(${card.id})"></i>
+        
+        </div>
+        `})
+                }
+            })
+        })
 };
 showPage()
 
-document.querySelector(".load").addEventListener("click", ()=>{
-    cardnum +=3;
+document.querySelector(".load").addEventListener("click", () => {
+    cardnum += 3;
     showPage()
 })
 
 //////////DELETE//////////
 
-  function deleteCard(id){
-        axios.delete(`http://localhost:3000/card/${id}`)
-        window.location.reload();
-    }
+function deleteCard(id) {
+    axios.delete(`http://localhost:3000/card/${id}`)
+    window.location.reload();
+}
 
 //////////UP-BUTTON//////////
 
-document.querySelector('.up').addEventListener('click', ()=>{
+document.querySelector('.up').addEventListener('click', () => {
     window.scroll({
         top: 0,
         behavior: 'smooth'
     })
 });
 
-window.addEventListener('scroll', ()=>{
-    if(window.scrollY > 200){
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 200) {
         document.querySelector('.up').style.right = "20px"
         document.querySelector('.add').style.bottom = "70px"
+        document.querySelector('.fav').style.bottom = "120px"
     }
-    else{
+    else {
         document.querySelector('.up').style.right = "-100px"
         document.querySelector('.add').style.bottom = "20px"
+        document.querySelector('.fav').style.bottom = "70px"
     }
 });
 
 //////////FAVORITES//////////
 
-function addFav(id){
-    console.log("test");
-    axios.get("http://localhost:3000/card/"+id)
-    .then(res => {
-        axios.post("http://localhost:3000/favs", res.data)
-    })
+function addFav(id) {
+    axios.get("http://localhost:3000/card/" + id)
+        .then(res => {  
+            axios.post("http://localhost:3000/favs", res.data)
+        })
 }
